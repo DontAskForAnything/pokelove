84e023c7685e75b226a2fd57f60a9f39318fef2c
DontAskForAnything <misiujasina@gmail.com>
2024-07-15 11:56:35 +0200

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { IdProvider } from "./utils/FavProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -15,6 +16,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <IdProvider>
           <App />
